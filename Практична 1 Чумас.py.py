427b36a535f455a71ae7a18e36de38a77bf998cc
Vlad Chumas <76513457+vladyslaw100@users.noreply.github.com>
2025-09-02 17:31:03 +0300

    Add files via upload

---
+++ Практична 1 Чумас.py.py
@@ -0,0 +1,32 @@
+age = 10
+kg = 72.4
+in_school = True
+name = 'Stanislav'
+money = 100.4
+toys = ['car', 'stick']
+is_funny = False
+houses = 0
+continent = 'Europe'
+friends = 2
+
+print(age)
+print(kg)
+print(in_school)
+print(name)
+print(money)
+print(toys)
+print(is_funny)
+print(houses)
+print(continent)
+print(friends)
+
+print(type(age))
+print(type(kg))
+print(type(in_school))
+print(type(name))
+print(type(money))
+print(type(toys))
+print(type(is_funny))
+print(type(houses))
+print(type(continent))
+print(type(friends))
