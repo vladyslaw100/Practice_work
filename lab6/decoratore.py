1bb192440969821ce0e66bae79c2b50521911b83
Vlad Chumas <76513457+vladyslaw100@users.noreply.github.com>
2025-11-12 09:11:19 +0200

    decoratore.py

--- lab6/decoratore.py
+++ lab6/decoratore.py
@@ -5,17 +5,17 @@ def checkcpu(func):
             a = args[0]
             b = args[1]
             cpu = args[2]
-
-        if cpu:
-            if a is not None and b is not None:
-                if a+b >= 100: cpu += 50
-                elif a+b >= 50: cpu += 30
-                elif a+b >= 25: cpu += 10
-            if cpu >= 50:
-                print(f'Не можу виконати це завдання — CPU перевищено: {cpu}%)')
-                return None
+            
+        if a is not None and b is not None:
+            if a+b >= 100: cpu += 50
+            elif a+b >= 50: cpu += 30
+            elif a+b >= 25: cpu += 10
+        if cpu >= 50:
+            print(f'Не можу виконати це завдання — CPU перевищено: {cpu}%)')
+            return None
             print("cpu:", cpu)
 
         return func(*args, **kwargs)
 
     return wrapper
+
